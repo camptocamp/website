34ea1e87949547f0178333cc24143d0a4ad72427
Denis Leemann <denis.leemann@camptocamp.com>
2017-01-06 12:12:56 +0100

    report invoice

--- odoo/local-src/specific_report/models/invoice.py
+++ odoo/local-src/specific_report/models/invoice.py
@@ -10,25 +10,17 @@ class InvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
     @api.multi
-    def product_id_change(self, product, uom_id, qty=0, name='',
-                          type='out_invoice', partner_id=False,
-                          fposition_id=False, price_unit=False,
-                          currency_id=False, company_id=None):
-        result = super(InvoiceLine, self).product_id_change(
-            product=product,
-            uom_id=uom_id,
-            qty=qty,
-            name=name,
-            type=type,
-            partner_id=partner_id,
-            fposition_id=fposition_id,
-            price_unit=price_unit,
-            currency_id=currency_id,
-            company_id=company_id,
-        )
-
-        name = result['value'] and result['value'].get('name')
+    def product_id_change(self):
+        vals = {}
+        if not self.product_id:
+            return {'domain': {'product_uom': []}}
+        result = super(InvoiceLine, self).product_id_change()
+        name = self.product_id.name_get()[0][1]
         if name:
-            result['value']['name'] = name.replace(u'] ', u']\n', 1)
+            name = name.replace(u'] ', u']\n', 1)
+            if self.product_id.description_sale:
+                name += '\n' + self.product_id.description_sale
+            vals['name'] = name
+            self.update(vals)
 
         return result
