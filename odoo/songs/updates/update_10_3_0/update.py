9246b00ad987dd72e5a2a9429555b27bd2796f3a
Denis Leemann <denis.leemann@camptocamp.com>
2017-02-08 15:00:42 +0100

    Update EN/FR/DE locale formatting

--- odoo/songs/updates/update_10_3_0/update.py
+++ odoo/songs/updates/update_10_3_0/update.py
@@ -30,8 +30,43 @@ def set_sales_settings(ctx):
 
 
 @anthem.log
+def update_lang_format(ctx):
+    langs = ['base.lang_fr', 'base.lang_de', 'base.lang_en']
+    vals = {
+        'thousands_sep': "'",
+        'grouping': '[3,0]',
+        'decimal_point': '.',
+        'date_format': '%d. %m. %Y',
+        'time_format': '%H:%M:%S',
+    }
+    for lang in langs:
+        res_lang = ctx.env.ref(lang)
+        res_lang.write(vals)
+
+
+@anthem.log
+def update_bvr_layout_settings(ctx):
+    company = ctx.env.ref('base.main_company')
+    vals = {
+        'bvr_add_horz': '0.15',
+        'bvr_add_vert': '-0.25',
+        'bvr_background': False,
+        'bvr_delta_horz': '0.00',
+        'bvr_delta_vert': '0.00',
+        'bvr_scan_line_front_size': '0',
+        'bvr_san_line_horz': '-0.04',
+        'bvr_san_letter_spacing': '0.00',
+        'bvr_san_line_vert': '0.04',
+        'merge_mode': 'in_memory'
+    }
+    company.write(vals)
+
+
+@anthem.log
 def main(ctx):
     """ Loading data """
     # reload_translation(ctx, 'specific_report')
     set_settings_proforma(ctx)
     set_sales_settings(ctx)
+    update_lang_format(ctx)
+    update_bvr_layout_settings(ctx)
