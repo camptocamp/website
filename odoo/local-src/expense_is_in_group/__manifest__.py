d303284c073aa7c820e4c9135db27ac3a658083e
oleksandrpaziuk <o.paziuk@mobilunity.com>
2017-09-19 13:45:56 +0300

    add readonly fields for expense for user not in A&F Accountant or Adviser groups

---
+++ odoo/local-src/expense_is_in_group/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Expanse with readonly fields',
+    'version': '10.0.0.0.0',
+    'category': 'Human Resources',
+    'depends': ['hr_expense'],
+    'data': [
+        'views/hr_expense_views.xml',
+    ],
+    'installable': True,
+}
